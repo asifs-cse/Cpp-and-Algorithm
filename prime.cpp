9e4ff3d53c77d08364a08494ece4df34c6b5f9f6
asifs-cse <asifs.cse@gmail.com>
2021-11-02 00:20:23 +0530

    update

--- prime.cpp
+++ prime.cpp
@@ -3,6 +3,24 @@ using namespace std;
 #include<conio.h>
 
 int main(){
-    cout<<"prime number";
+    
+    int i,n;
+    cout<<"Input any integer number: ";
+    cin>>n;
+
+    for (i = 2; i < n; i++)
+    {
+        if (n%i==0)
+        {
+            cout<<"Prime nmber ";
+            break;
+        }
+        
+    }
+    if (i==n)
+    {
+        cout<<"Not prime number ";
+    }
+    
     return 0;
 }
