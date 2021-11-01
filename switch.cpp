467297abec3ba2a144dd54020030a54b1c9e1198
asifs-cse <asifs.cse@gmail.com>
2021-11-02 00:37:35 +0530

    start switch case

---
+++ switch.cpp
@@ -0,0 +1,22 @@
+using namespace std;
+#include<iostream>
+#include<conio.h>
+int main(){
+    char n;
+    cout<<"Enter a cheracter:";
+    cin>>n;
+
+    switch (n)
+    {
+    case 'a':
+        cout<<"enter a";
+        break;
+        case 'b':
+        cout<<"enter b";
+    
+    default:
+        break;
+    }
+
+    return 0;
+}
