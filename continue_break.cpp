54ec911d5becb4c350646e75493b2e5bdc4b610a
asifs-cse <asifs.cse@gmail.com>
2021-11-01 23:37:46 +0530

    using continue

---
+++ continue_break.cpp
@@ -0,0 +1,17 @@
+using namespace std;
+#include<iostream>
+#include<conio.h>
+int main(){
+    
+    for (int i = 1; i < 100; i++)
+    {
+        if (i%3==0)
+        {
+            continue;  
+        }
+        cout<<i<<endl;
+        
+    }
+    
+    return 0;
+}
