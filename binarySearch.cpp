f448420f1587a25fbeae4c3809b4f5edf6e1cc03
asifs-cse <asifs.cse@gmail.com>
2022-01-29 07:21:42 +0530

    struct

---
+++ binarySearch.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int binarySearch(int arr[],int n,int f){
+    
+}
+
+int main(){
+    int n, f;
+    cout<<"Enter array item: "<<endl;
+    cin>>n;
+    int arr[5];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"Enter find value: "<<endl;
+    cout<<binarySearch(arr[],n,f)<<endl;
+    
+
+    return 0;
+}
---
+++ struct.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct myStruct{
+    string name;
+    int age;
+    double height;
+};
+int main(){
+    struct myStruct s1, s2;
+    s1.name="Asif";
+    s1.age=22;
+    s1.height = 5.10;
+    cout<<"Name: "<<s1.name<<"\n"<<"Age: "<<s1.age<<"\n"<<"Age: "<<s1.age;
+    return 0;
+}
